52b722d84da37012f0c56031973b4de10a55b193
Ilija Matoski <ilijamt@gmail.com>
2023-10-10 10:17:13 +0200

    Setup for testing

---
+++ internal/provider/provider_integration_test.go
@@ -0,0 +1,64 @@
+package provider_test
+
+import (
+	d "github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	r "github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/ilijamt/terraform-provider-awx/internal/provider"
+	"github.com/ilijamt/terraform-provider-awx/version"
+	"testing"
+)
+
+// testAccProtoV6ProviderFactoriesUnique is used to ensure that the provider instance used for
+// each acceptance test is unique.
+// This is necessary because this provider make use of state stored in the provider instance.
+func testAccProtoV6ProviderFactoriesUnique() map[string]func() (tfprotov6.ProviderServer, error) {
+	var resources = func() []func() r.Resource {
+		return []func() r.Resource{}
+	}
+	var dataSources = func() []func() d.DataSource {
+		return []func() d.DataSource{}
+	}
+
+	return map[string]func() (tfprotov6.ProviderServer, error){
+		"awx": providerserver.NewProtocol6WithError(provider.New(version.Version, resources(), dataSources())()),
+	}
+}
+
+// testAccPreCheck ensures that the environment is properly configured for acceptance testing.
+func testAccPreCheck(t *testing.T) {
+	// You can add code here to run prior to any test case execution, for example assertions
+	// about the appropriate environment variables being set are common to see in a pre-check
+	// function.
+}
+
+const (
+	providerConfig = `
+provider "awx" {
+  username             = "admin"
+  password             = "admin"
+  host                 = "http://localhost"
+  insecure_skip_verify = "true"
+}
+`
+)
+
+func TestProviderIntegration(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		IsUnitTest:               true,
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactoriesUnique(),
+		ExternalProviders:        map[string]resource.ExternalProvider{},
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig,
+				Check: func(state *terraform.State) error {
+					return nil
+				},
+			},
+		},
+	})
+}
--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -12,41 +12,14 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
 
-func resources() []func() resource.Resource {
-	return []func() resource.Resource{}
-}
-
-func dataSources() []func() datasource.DataSource {
-	return []func() datasource.DataSource{}
-}
-
-// testAccProtoV6ProviderFactories are used to instantiate a provider during
-// acceptance testing. The factory function will be invoked for every Terraform
-// CLI command executed to create a provider server to which the CLI can
-// reattach.
-var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"awx": providerserver.NewProtocol6WithError(provider.New("test", resources(), dataSources())()),
-}
-
-const (
-	providerConfig = `
-provider "awx" {
-  username             = "admin"
-  password             = "admin"
-  host     			   = "http://localhost"
-  insecure_skip_verify = true"
-}
-`
-)
-
-func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
-}
-
 func TestProvider(t *testing.T) {
-	awxProvider := testAccProtoV6ProviderFactories["awx"]
+	var resources = func() []func() resource.Resource {
+		return []func() resource.Resource{}
+	}
+	var dataSources = func() []func() datasource.DataSource {
+		return []func() datasource.DataSource{}
+	}
+	awxProvider := providerserver.NewProtocol6WithError(provider.New("test", resources(), dataSources())())
 	require.NotNil(t, awxProvider)
 	frameworkServer, err := awxProvider()
 	require.NoError(t, err)
