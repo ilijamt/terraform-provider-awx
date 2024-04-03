d96692ccd531cc85fcc7d1bc520e82827e974650
Ilija Matoski <ilijamt@gmail.com>
2024-04-03 18:29:08 +0200

    tools: added a generate build command to automate the build process for the providers

---
+++ tools/generator/cmd/generator/cmd/gen-builds.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/ilijamt/terraform-provider-awx/tools/generator/internal"
+)
+
+var genBuilds = &cobra.Command{
+	Use:   "gen-builds [build-config-file (yaml)]",
+	Args:  cobra.ExactArgs(1),
+	Short: "Generate all the builds for the versions we manage",
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		var buildConfigFile = args[0]
+		var buildConfig = new(internal.BuildConfig)
+		if err = buildConfig.Load(buildConfigFile); err != nil {
+			return err
+		}
+		var dryRun, _ = cmd.Flags().GetBool("dry-run")
+		var cmds []string
+		for _, b := range *buildConfig {
+			func(b *internal.BuildConfigVersion) {
+				defer func() {
+					if !dryRun {
+						b.Inc()
+					}
+				}()
+				v := b.GetBuildVersion()
+				cmds = append(cmds,
+					fmt.Sprintf("make generate build VERSION=%s", b.Version),
+					"git add internal docs",
+					fmt.Sprintf("git commit -m'chore: generated version %s with tag v%s'", b.Version, b.GetBuildVersion()),
+					fmt.Sprintf("git tag v%s", v),
+					fmt.Sprintf("git push origin refs/tags/v%s", v),
+				)
+			}(b)
+		}
+
+		if !dryRun {
+			err = buildConfig.Save(buildConfigFile)
+			if err != nil {
+				return err
+			}
+		}
+
+		cmds = append(cmds,
+			"git add versions.yaml",
+			"git commit -m'chore: updated versions.yaml'",
+		)
+
+		_, _ = fmt.Fprintln(cmd.OutOrStdout(), strings.Join(cmds, "\n"))
+
+		return nil
+
+	},
+}
+
+func init() {
+	genBuilds.Flags().BoolP("dry-run", "n", false, "Do not increase the build counters")
+	rootCmd.AddCommand(genBuilds)
+}
---
+++ tools/generator/internal/build.go
@@ -0,0 +1,49 @@
+package internal
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type BuildConfigVersion struct {
+	Version string `json:"version" yaml:"version"`
+	Build   uint8  `json:"build" yaml:"build"`
+}
+
+func (c *BuildConfigVersion) Inc() {
+	c.Build += 1
+}
+
+func (c *BuildConfigVersion) GetBuildVersion() (build string) {
+	return fmt.Sprintf("%s-%d", c.Version, c.Build)
+}
+
+type BuildConfig []*BuildConfigVersion
+
+func (c *BuildConfig) GetBuildVersion(ver string) (build string, err error) {
+	for _, v := range *c {
+		if v.Version == ver {
+			return v.GetBuildVersion(), nil
+		}
+	}
+	return "", fmt.Errorf("version not found")
+}
+
+func (c *BuildConfig) Load(filename string) error {
+	var payload, err = os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	err = yaml.Unmarshal(payload, &c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *BuildConfig) Save(filename string) error {
+	var payload, _ = yaml.Marshal(c)
+	return os.WriteFile(filename, payload, 0655)
+}
