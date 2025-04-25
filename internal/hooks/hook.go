6ab3fa41be20ae6db5eaebda9dd3cca24b9eebfc
Ilija Matoski <ilijamt@gmail.com>
2025-04-26 00:04:06 +0900

    update resources

--- internal/hooks/hook.go
+++ internal/hooks/hook.go
@@ -1,5 +1,7 @@
 package hooks
 
-import "context"
+import (
+	"context"
+)
 
 type Hook[T any] func(ctx context.Context, apiVersion string, source Source, callee Callee, orig, state T) (err error)
--- internal/resource/credential_type_id.go
+++ internal/resource/credential_type_id.go
@@ -2,6 +2,9 @@ package resource
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 
@@ -10,5 +13,18 @@ import (
 
 func GetCredentialTypeID(ctx context.Context, client client.Client, rci CallInfo, name string) (d diag.Diagnostics, err error) {
 	d = make(diag.Diagnostics, 0)
+	if client == nil {
+		err = errors.Join(err, fmt.Errorf("client is nil"))
+	}
+	name = strings.TrimSpace(name)
+	if name == "" {
+		err = errors.Join(err, fmt.Errorf("name is empty"))
+	}
+
+	if err != nil {
+		d.AddError("unable to fetch credential type id", err.Error())
+		return d, err
+	}
+
 	return d, err
 }
